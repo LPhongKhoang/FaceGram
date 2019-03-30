27bde5d4f2371f32b01189c151fcb61a510ae603
LongPKPro <vulongkm1998@gmail.com>
2019-03-30 13:59:39 +0700

    Create frontend upload post

---
+++ FaceGram/Models/UploadPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FaceGram.Models
+{
+    public class UploadPostModel
+    {
+        [Required()]
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+    }
+}
